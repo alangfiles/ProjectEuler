ad4f6a07de40985da84598e888b330977680aa07
Alan Files <alangfiles@users.noreply.github.com>
2023-01-14 07:23:42 -0500

    comments

--- problem15.js
+++ problem15.js
@@ -2,8 +2,12 @@
 // "how many 40 character strings can you make from 20 'D' and 20 'R's?"
 // there's probably a good way to actually generate these / walk through them,
 // but I wanna just hit and run so let's generate new paths and sample them
+// seems like you also have to only generate half the paths then double it.
+// [ 'DDRR', 'DRDR', 'DRRD', 'RDDR', 'RDRD', 'RRDD' ]
+// 'RDDR', 'RDRD', 'RRDD' are just inverse of 'DDRR', 'DRDR', 'DRRD'
+// probably a good way to generate all those paths.....
 
-//let's have some fun with sampling.
+// but let's have some fun with sampling.
 function generate_random_path(dimension) {
   //every path is the same number of steps, 2*dimension (this makes sense
   // cause you can only go down dimesion times, then right dimesion times)
